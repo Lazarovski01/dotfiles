2595ca6858d8d036ebe66631d368b85475fde6a8
Lazarovski01 <lazarposao01@gmail.com>
2025-09-09 15:09:26 +0200

    Change to the install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "Hello World!" > /tmp/hello
+
+echo "if ! infocmp alacritty >/dev/null 2>&1; then; export TERM=xterm-256color; fi" >> ~/.bashrc
+
+echo "stty erase ^? 2>/dev/null || stty erase ^H 2>/dev/null" >> ~/.bashrc
