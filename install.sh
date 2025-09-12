7cb3161b4c69bf8617b52d82c66b53f7c2316c6a
Lazarovski01 <lazarposao01@gmail.com>
2025-09-12 11:26:31 +0000

    changin install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-echo "Hello World alee!" > /tmp/hello
+set -euo pipefail
 
-cat >> ~/.bashrc <<'EOF'
-# --- DevPod terminal sanity (TERM & backspace) ---
-if ! infocmp alacritty >/dev/null 2>&1; then
-  export TERM=xterm-256color
+if ! command -v chezmoi >/dev/null; then
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/Lazarovski01/dotfiles.git
 fi
-if [[ $- == *i* ]] && [ -t 0 ]; then
-  stty erase ^? 2>/dev/null || stty erase ^H 2>/dev/null
-fi
-# --- end ---
-EOF
 
-
-echo "alias ll='ls -la --color=auto'" >> ~/.bashrc
-echo "set -o vi" >> ~/.bashrc
-
+exit 0
